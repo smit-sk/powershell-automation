fbef367da4a5e68e5e00806a12c43a5cb8be2665
Smit Khokhariya <68110421+smit-sk@users.noreply.github.com>
2024-11-08 20:21:13 -0500

    Update HelloWorld.ps1

--- HelloWorld.ps1
+++ HelloWorld.ps1
@@ -1,3 +1,15 @@
+# Load the JSON credentials from an environment variable
+$creds = $env:AZURE_CREDENTIALS | ConvertFrom-Json
+
+# Log in to Azure with the Service Principal credentials
+$securePassword = ConvertTo-SecureString $creds.clientSecret -AsPlainText -Force
+$psCredential = New-Object System.Management.Automation.PSCredential ($creds.clientId, $securePassword)
+Connect-AzAccount -ServicePrincipal -Credential $psCredential -Tenant $creds.tenantId
+
+# Set the subscription context to avoid the "No subscription found" error
+Set-AzContext -SubscriptionId $creds.subscriptionId
+
+# Your VM creation script
 $resourceGroupName = "Project-07-RG"
 $location = "canadacentral"
 $vmName = "Powershell-VM"
@@ -5,9 +17,8 @@ $adminUsername = "azureuser"
 $adminPassword = ConvertTo-SecureString "Smit@7008" -AsPlainText -Force
 
 New-AzVm -ResourceGroupName $resourceGroupName `
-	-Location $location `
-	-Name $vmName `
-	-Credential (New-Object System.Management.Automation.PSCredential -ArgumentList $adminUsername, $adminPassword) `
-	-Image "Ubuntu2204" `
-	-PublicIpAddressName "$vmName-publicIP"
-
+    -Location $location `
+    -Name $vmName `
+    -Credential (New-Object System.Management.Automation.PSCredential -ArgumentList $adminUsername, $adminPassword) `
+    -Image "Ubuntu2204" `
+    -PublicIpAddressName "$vmName-publicIP"
